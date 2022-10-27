acc472adaf50a0adcb87dc00dc311d625872f646
nbert71 <nicobert2205@gmail.com>
2022-10-27 16:49:35 +0200

    add docker-compose and User entity

---
+++ src/main/java/com/example/javacrudapi/entity/User.java
@@ -0,0 +1,24 @@
+package com.example.javacrudapi.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String username;
+    private String email;
+    private String firstName;
+    private String lastName;
+    private Integer promo;
+}
