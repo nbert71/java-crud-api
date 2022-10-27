75a59d7b711da8abf47434d9fc1ee232dd073a91
nbert71 <nicobert2205@gmail.com>
2022-10-27 16:49:17 +0200

    Initial commit

---
+++ src/main/java/com/example/javacrudapi/JavaCrudApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.javacrudapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaCrudApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaCrudApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/javacrudapi/JavaCrudApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javacrudapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaCrudApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
